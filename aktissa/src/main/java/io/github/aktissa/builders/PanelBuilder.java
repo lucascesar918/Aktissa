d444865aa7d5ad51e289e963e8954e149056cea7
Lucas César <o@o>
2026-06-19 14:52:23 -0300

    add: espaçamento automático entre componentes

--- aktissa/src/main/java/io/github/aktissa/builders/PanelBuilder.java
+++ aktissa/src/main/java/io/github/aktissa/builders/PanelBuilder.java
@@ -2,48 +2,56 @@ package io.github.aktissa.builders;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 
 import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
 import javax.swing.border.TitledBorder;
 
-import io.github.aktissa.theme.Theme;
 import io.github.aktissa.theme.ThemeManager;
 
 public class PanelBuilder {
     private final JPanel component;
+    private int currentAxis = -1;
 
     public PanelBuilder() {
         this.component = new JPanel();
         this.component.setBackground(ThemeManager.current().backgroundBase());
+        this.component.setBorder(BorderFactory.createEmptyBorder(1, 1, 1, 1));
     }
 
     public PanelBuilder vertical() {
-        this.component.setLayout(new BoxLayout(this.component, BoxLayout.Y_AXIS));
+        this.currentAxis = BoxLayout.Y_AXIS;
+        this.component.setLayout(new BoxLayout(this.component, this.currentAxis));
         return this;
     }
 
     public PanelBuilder horizontal() {
-        this.component.setLayout(new BoxLayout(this.component, BoxLayout.X_AXIS));
+        this.currentAxis = BoxLayout.X_AXIS;
+        this.component.setLayout(new BoxLayout(this.component, this.currentAxis));
         return this;
     }
 
     public PanelBuilder flow() {
-        this.component.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
+        this.currentAxis = -1;
+        this.component.setLayout(new FlowLayout(FlowLayout.LEFT, 15, 15));
         return this;
     }
 
     public PanelBuilder grid(int rows, int cols) {
-        this.component.setLayout(new GridLayout(rows, cols, 5, 5));
+        this.currentAxis = -1;
+        this.component.setLayout(new GridLayout(rows, cols, 15, 15));
         return this;
     }
 
     public PanelBuilder border() {
-        this.component.setLayout(new BorderLayout(5, 5));
+        this.currentAxis = -1;
+        this.component.setLayout(new BorderLayout(15, 15));
         return this;
     }
 
@@ -51,13 +59,24 @@ public class PanelBuilder {
         TitledBorder titledBorder = BorderFactory.createTitledBorder(ThemeManager.current().defaultLineBorder(), text);
         titledBorder.setTitleColor(ThemeManager.current().textSecondary());
         
-        Border padding = BorderFactory.createEmptyBorder(5, 5, 5, 5);
+        Border padding = BorderFactory.createEmptyBorder(10, 10, 10, 10);
         this.component.setBorder(BorderFactory.createCompoundBorder(titledBorder, padding));
         
         return this;
     }
 
+    private void injectAutoSpacing() {
+        if (this.component.getComponentCount() > 0) {
+            if (this.currentAxis == BoxLayout.Y_AXIS) {
+                this.component.add(Box.createRigidArea(new Dimension(0, 15)));
+            } else if (this.currentAxis == BoxLayout.X_AXIS) {
+                this.component.add(Box.createRigidArea(new Dimension(15, 0)));
+            }
+        }
+    }
+
     public PanelBuilder add(Component child) {
+        injectAutoSpacing();
         this.component.add(child);
         return this;
     }
@@ -68,6 +87,7 @@ public class PanelBuilder {
     }
 
     public PanelBuilder add(ButtonBuilder builder) {
+        injectAutoSpacing();
         this.component.add(builder.build());
         return this;
     }
@@ -78,6 +98,7 @@ public class PanelBuilder {
     }
 
     public PanelBuilder add(TextFieldBuilder builder) {
+        injectAutoSpacing();
         this.component.add(builder.build());
         return this;
     }
@@ -88,6 +109,7 @@ public class PanelBuilder {
     }
 
     public PanelBuilder add(PanelBuilder builder) {
+        injectAutoSpacing();
         this.component.add(builder.build());
         return this;
     }
